c1e82d5fe4c0b5e60a671dbd07e4b5c3fc42f6cd
Rylmar Quispe <rylm.fqt28@gmail.com>
2022-04-12 22:20:05 -0400

    Implementacion clase Responsive en la pantalla de bienvenida

--- lib/ui/screen/welcome_screen.dart
+++ lib/ui/screen/welcome_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:cochabambacultural/ui/widgets/general_button.dart';
 import 'package:cochabambacultural/utils/app_colors.dart';
+import 'package:cochabambacultural/utils/responsive.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -10,6 +11,7 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colorApp = AppColors();
+    final Responsive responsive = Responsive.of(context);
     return Scaffold(
       body: Container(
         color: colorApp.primaryBackground,
@@ -18,37 +20,39 @@ class WelcomeScreen extends StatelessWidget {
             Center(
                 child: ListView(
               children: [
-                const SizedBox(height: 40),
+                SizedBox(height: responsive.dp(3)),
                 SvgPicture.asset(
                   'assets/Logo-app.svg',
-                  height: 300,
-                  width: 300,
+                  height: responsive.hp(38),
+                  width: responsive.wp(55),
+                  //height: 300,
+                  //width: 300,
                   alignment: Alignment.topCenter,
                 ),
-                const SizedBox(height: 60),
+                SizedBox(height: responsive.hp(8)),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 0),
                   child: Text(
                     'BIENVENIDO',
-                    textAlign: TextAlign.left,
+                    textAlign: TextAlign.center,
                     style: TextStyle(
                       color: colorApp.primaryFont,
-                      fontSize: 35,
+                      fontSize: responsive.dp(3.9),
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
+                SizedBox(
+                  height: responsive.hp(2),
                 ),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 0),
                   child: Text(
-                    'Encuentra diferentes eventos culturales que se llevan a cabo en Cochabamba y mantente informado sobre tus eventos favoritos.',
-                    textAlign: TextAlign.left,
+                    'Encuentra diferentes eventos culturales que\nse llevan a cabo en Cochabamba y mantente\ninformado sobre tus eventos favoritos.',
+                    textAlign: TextAlign.center,
                     style: TextStyle(
                       color: colorApp.secundaryFont,
-                      fontSize: 18,
+                      fontSize: responsive.dp(1.9),
                       fontWeight: FontWeight.normal,
                     ),
                   ),
