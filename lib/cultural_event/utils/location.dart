0a257a63831a87f459a9af6986b8e56a11079b15
Rylmar Quispe <rylm.fqt28@gmail.com>
2022-10-20 22:28:42 -0400

    Cambio concatenacion de direccion a retornar

--- lib/cultural_event/utils/location.dart
+++ lib/cultural_event/utils/location.dart
@@ -6,12 +6,12 @@ class Location {
     List<Placemark> place =
         await placemarkFromCoordinates(location.latitude, location.longitude);
 
-    String locality = place[0].locality == null ? "" : '${place[0].locality!},';
+    String locality = place[0].locality == null ? "" : place[0].locality!;
     String subAdministrativeArea = place[0].subAdministrativeArea == null
         ? ""
-        : '${place[0].subAdministrativeArea!},';
+        : ', ${place[0].subAdministrativeArea!}';
     String thoroughfare =
-        place[0].thoroughfare == null ? "" : place[0].thoroughfare!;
+        place[0].thoroughfare == null ? "" : ', ${place[0].thoroughfare!}';
 
     return '$locality $subAdministrativeArea $thoroughfare';
   }
