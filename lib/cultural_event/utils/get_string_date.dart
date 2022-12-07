899fca9524ee524d2ca09390d118649805d406ac
Rylmar Quispe <rylm.fqt28@gmail.com>
2022-12-06 21:07:35 -0400

    Correcion visualizacion de la hora

--- lib/cultural_event/utils/get_string_date.dart
+++ lib/cultural_event/utils/get_string_date.dart
@@ -4,7 +4,9 @@ class GetStringDate {
     dateFormat += '${date.day}/';
     dateFormat += '${date.month}/';
     dateFormat += '${date.year}  ';
-    dateFormat += '${date.hour}:';
+
+    String hour = (date.hour < 10) ? '0${date.hour}' : '${date.hour}';
+    dateFormat += '$hour:';
 
     String minute = (date.minute < 10) ? '0${date.minute}' : '${date.minute}';
     dateFormat += minute;
